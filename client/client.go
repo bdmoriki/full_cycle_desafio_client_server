4cfabb8dead1f011e0eb5cb68eda7bb1d0ec2800
bdmoriki <dupim1@hotmail.com>
2024-07-16 17:51:41 -0300

    Adicionando client e inclusao da gravacao da cotacao no server

---
+++ client/client.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const ENDPOINT_SERVER_COTACACAO = "http://localhost:8080/cotacao"
+
+type Cotacao struct {
+	Bid string `json:"bid"`
+}
+
+func main() {
+	// Utilizando o package "context", o client.go terá um timeout máximo de 300ms para receber o resultado do server.go
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", ENDPOINT_SERVER_COTACACAO, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			// Retornar erro nos logs caso o tempo de execução seja insuficiente.
+			log.Println("Falha ao consultar o server de cotação pois a chamada ultrapassou os 300ms permitidos.")
+		}
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var c Cotacao
+	err = json.Unmarshal([]byte(body), &c)
+	if err != nil {
+		log.Printf("Falha ao codificar a resposta : %v", err)
+		panic(err)
+	}
+
+	// O client.go terá que salvar a cotação atual em um arquivo "cotacao.txt" no formato: Dólar: {valor}
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		fmt.Printf("Erro ao criar o arquivo: %v\n", err)
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("Dólar: %s", c.Bid))
+	if err != nil {
+		fmt.Printf("Erro ao criar o arquivo: %v\n", err)
+		panic(err)
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+const ENDPOINT_COTACAO = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+
+var DB *gorm.DB
+
+type CotacaoResponse struct {
+	Bid string `json:"bid"`
+}
+
+type Cotacao struct {
+	USDBRL struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USDBRL"`
+}
+
+type CotacaoTable struct {
+	Code       string `gorm:"primaryKey"`
+	Codein     string
+	Name       string
+	High       string
+	Low        string
+	VarBid     string
+	PctChange  string
+	Bid        string
+	Ask        string
+	Timestamp  string
+	CreateDate string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/cotacao", CotacaoHandler)
+
+	var err error
+	DB, err = gorm.Open(sqlite.Open(":memory:"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic(err)
+	}
+	DB.AutoMigrate(&CotacaoTable{})
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func CotacaoHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	cotacao, err := buscarCotacao(ctx)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = gravarCotacao(ctx, cotacao)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(CotacaoResponse{Bid: cotacao.USDBRL.Bid})
+}
+
+func buscarCotacao(ctx context.Context) (*Cotacao, error) {
+	// O timeout máximo para chamar a API de cotação do dólar deverá ser de 200ms
+	ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", ENDPOINT_COTACAO, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			// Retornar erro nos logs caso o tempo de execução seja insuficiente.
+			log.Println("Falha ao consultar a cotação pois a chamada ultrapassou os 200ms permitidos.")
+		}
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var c Cotacao
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func gravarCotacao(ctx context.Context, cotacao *Cotacao) error {
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Millisecond)
+	defer cancel()
+
+	err := DB.WithContext(ctx).Create(&CotacaoTable{
+		Code:       cotacao.USDBRL.Code,
+		Codein:     cotacao.USDBRL.Codein,
+		Name:       cotacao.USDBRL.Name,
+		High:       cotacao.USDBRL.High,
+		Low:        cotacao.USDBRL.Low,
+		VarBid:     cotacao.USDBRL.VarBid,
+		PctChange:  cotacao.USDBRL.PctChange,
+		Bid:        cotacao.USDBRL.Bid,
+		Ask:        cotacao.USDBRL.Ask,
+		Timestamp:  cotacao.USDBRL.Timestamp,
+		CreateDate: cotacao.USDBRL.CreateDate,
+	}).Error
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			// Retornar erro nos logs caso o tempo de execução seja insuficiente.
+			log.Println("Falha ao inserir as informações de cotação pois a operação ultrapassou os 10ms permitidos.")
+		}
+		log.Println("Falha", err)
+		return err
+	}
+
+	var cotacoes []CotacaoTable
+	DB.Find(&cotacoes)
+
+	for _, c1 := range cotacoes {
+		fmt.Println(c1)
+	}
+
+	return nil
+}
